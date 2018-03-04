5dedd5a5798a72623872a70d4d9b0123c3efa5b7
Andreas Haberberger <andreas@haberberger.com>
2018-03-04 20:52:21 +0100

    React to POST

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -6,6 +6,7 @@ use App\Entity\Thingy;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Class ApiController
@@ -15,7 +16,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class ApiController extends Controller
 {
     /**
-     * @Route("/thingy/{id}", name="api-thingy", requirements={"id"="\d+"})
+     * @Route("/thingy/{id}", name="api-thingy", requirements={"id"="\d+"}, methods={"GET"})
      * @param integer $id
      * @return JsonResponse
      * @throws \LogicException
@@ -23,6 +24,30 @@ class ApiController extends Controller
     public function getThingy($id)
     {
         $thingy = $this->getDoctrine()->getRepository(Thingy::class)->find($id);
+
+        return new JsonResponse($thingy->toArray());
+    }
+
+    /**
+     * @Route("/thingy", name="api-post-thingy", methods={"POST"})
+     * @return JsonResponse
+     * @throws \LogicException
+     */
+    public function postThingy(Request $request)
+    {
+        $thingy = new Thingy();
+        $content = $request->getContent();
+        
+        if (!empty($content))
+        {
+            $params = json_decode($content, true);
+        }
+        $thingy->setText($params['text']);
+        $thingy->setNumber($params['number']);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($thingy);
+        $em->flush();
 
         return new JsonResponse($thingy->toArray());
     }
